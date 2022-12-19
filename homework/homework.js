9fa478417924bf065952dff7b5d50107223f1ca8
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-20 02:19:56 +0900

    a

--- homework/homework.js
+++ homework/homework.js
@@ -1,19 +1,4 @@
 let element = document.createElement("button"); // <button></button> ができる
 element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
-let target = document.querySelector("#bb-rankHead__title"); // 追加したい要素を見つけてくる
+let target = document.querySelector("#green"); // 追加したい要素を見つけてくる
 target.appendChild(element); // 追加する
-
-
-// function f(n){
-//     let cs = n.childNodes;
-//     for(let i = 0; i < cs.length; i++){
-//       let c = cs[i];
-//       if(c.nodeType == Node.TEXT_NODE){
-//         c.textContent = c.textContent.replace(/前期/g, "大山");
-//       }
-//       else{
-//         f(c);
-//       }
-//     }
-//   }
-//   f(document.body); // document.body は HTML の文章全体を示す要素
