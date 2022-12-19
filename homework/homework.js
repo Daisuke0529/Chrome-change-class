e6240c9e5a4f0331cb458e7a111da985acc50c9f
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-20 01:51:18 +0900

    c

--- homework/homework.js
+++ homework/homework.js
@@ -1,4 +1,20 @@
-let element = document.createElement("button"); // <button></button> ができる
-element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
-let target = document.querySelector("#yui_3_17_2_1_1671467138280_30"); // 追加したい要素を見つけてくる
-target.appendChild(element); // 追加する
+// let element = document.createElement("button"); // <button></button> ができる
+// element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
+// let target = document.querySelector("#yui_3_17_2_1_1671467138280_30"); // 追加したい要素を見つけてくる
+// target.appendChild(element); // 追加する
+
+
+function f(n){
+    let cs = n.childNodes;
+    for(let i = 0; i < cs.length; i++){
+      let c = cs[i];
+      if(c.nodeType == Node.TEXT_NODE){
+        c.textContent = c.textContent.replace(/前期/g, "大山");
+      }
+      else{
+        f(c);
+      }
+    }
+  }
+  
+  f(document.body); // document.body は HTML の文章全体を示す要素
