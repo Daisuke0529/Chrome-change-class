b4b8343352e0f90d1cdcd29d22ce146246b79002
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-20 02:25:37 +0900

    q

--- homework/homework.js
+++ homework/homework.js
@@ -1,4 +1,10 @@
-let element = document.createElement("button"); // <button></button> ができる
-element.textContent = "Push Me!";               // <button>Push Me!</button> ができる
-let target = document.querySelector("#green"); // 追加したい要素を見つけてくる
-target.appendChild(element); // 追加する
+let element1 = document.createElement("button"); // <button></button> ができる
+element1.textContent = "Push Me!";               // <button>Push Me!</button> ができる
+let target1 = document.querySelector("#green"); // 追加したい要素を見つけてくる
+target1.appendChild(element1); // 追加する
+
+
+let element2 = document.createElement("button"); // <button></button> ができる
+element2.textContent = "Push Me!";               // <button>Push Me!</button> ができる
+let target2 = document.querySelector("#page-header-headings"); // 追加したい要素を見つけてくる
+target2.appendChild(element2); // 追加する
