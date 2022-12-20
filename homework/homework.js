28b624175ec359da18998e5fb5ee4a4ce93ebe81
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-20 10:38:26 +0900

    a

--- homework/homework.js
+++ homework/homework.js
@@ -6,5 +6,5 @@ target1.appendChild(element1); // 追加する
 
 let element2 = document.createElement("button"); // <button></button> ができる
 element2.textContent = "Push Me!";               // <button>Push Me!</button> ができる
-let target2 = document.querySelector("#yui_3_17_2_1_1671470841257_30"); // 追加したい要素を見つけてくる
+let target2 = document.querySelector("#yui_3_17_2_1_1671471020449_30"); // 追加したい要素を見つけてくる
 target2.appendChild(element2); // 追加するyui_3_17_2_1_1671470841257_30
