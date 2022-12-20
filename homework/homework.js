46d1bf324487b7f5cdd6549c5304fbf742863fdc
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-20 14:23:23 +0900

    あ

--- homework/homework.js
+++ homework/homework.js
@@ -13,7 +13,7 @@ for(let i = 0; i < es.length; i++){
   if(e && e.textContent.includes("前期")){
     e.style.display = 'none';
   }
-  if(e && e.textContent.includes("第１クォーター")){
+  if(e && e.textContent.includes("第1クォーター")){
     e.style.display = 'none';
   }
 }
