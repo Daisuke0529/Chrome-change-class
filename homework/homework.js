e0356f22e5f9ceb150e7e8d7e234a0167a0ff792
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-20 14:18:00 +0900

    v

--- homework/homework.js
+++ homework/homework.js
@@ -1,6 +1,21 @@
 
-
 let element2 = document.createElement("button"); // <button></button> ができる
 element2.textContent = "Push Me!";               // <button>Push Me!</button> ができる
 let target2 = document.querySelector("#site-news-forum"); // 追加したい要素を見つけてくる
 target2.appendChild(element2); // 追加するyui_3_17_2_1_1671470841257_30
+
+
+
+let ab=document.querySelectorAll(".tree_item");
+for(let i=0;i<ab.length;i++){
+    let bb=ab[i];
+    bb.style.display='none';
+}
+
+let es = document.querySelectorAll(".tree_item");
+for(let i = 0; i < es.length; i++){
+  let e = es[i];
+  if(e && e.textContent.includes("前期")){
+    es.style.display = 'none';
+  }
+}
