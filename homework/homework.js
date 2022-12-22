7c46874ab56ba4971160856417619cecd7f00b3f
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-22 11:32:04 +0900

    a

--- homework/homework.js
+++ homework/homework.js
@@ -20,3 +20,22 @@ for(let i = 0; i < es.length; i++){
     e.style.display = 'none';
   }
 }
+
+let element1 = document.createElement("script"); // <button></button> ができる 
+element1.src = "https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js";         // <button>Push Me!</button> ができる
+let target1 = document1.querySelector("head"); // 追加したい要素を見つけてくる
+target1.appendChild(element1);
+
+let element3 = document.createElement("script"); // <button></button> ができる 
+element3.src = "https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js";         // <button>Push Me!</button> ができる
+let target3 = document3.querySelector("head"); // 追加したい要素を見つけてくる
+target3.appendChild(element3);
+
+
+
+function draw(){
+    background(245,245,220);
+    fill(255);
+    strokeWeight(3);
+    ellipse(width/2,height/2,15);
+}
