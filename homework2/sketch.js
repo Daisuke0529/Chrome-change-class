9a84eae0693b6e01510fd3d9be07a61d2688d876
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-24 18:52:36 +0900

    a

--- homework2/sketch.js
+++ homework2/sketch.js
@@ -7,19 +7,23 @@ target1.appendChild(element1);
 let time;
 
 function setup() {
-  let cnv = createCanvas(1000, 100);
+  let cnv = createCanvas(windowWidth, windowHeight);
   cnv.position(0, 0, 'fixed');
   time=0;
 }
 
 function draw(){
-    background(245,245,220);
+    background('rgba(245,245,220,0)');
     textSize(100);
     fill(255,255,0);
-    text("行けてるで",20,height/2);
-    if(time%10==0){console.log(time);}
+    textAlign(CENTER);
+    text("行けてるで",width/2,height/2);
+    if(time%10==0&&time<2000){console.log(time);}
     time+=1;
 }
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 
