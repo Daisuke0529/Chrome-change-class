b71d2b4b11e07ef8304d74a0db17d890a0f02433
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-26 03:21:15 +0900

    a

--- tigers/content_script2.js
+++ tigers/content_script2.js
@@ -2,7 +2,7 @@ let element1 = document.createElement("button");
 element1.classList.add("refresh");
 let element2 =document.createElement("a");
 element2.href="https://www.youtube.com/watch?v=Jb1_rM-qHzY&t=263s";
-element2.textContent = "Refresh!";            
+element2.textContent = "3:大山悠輔";            
 let target = document.querySelector("h1"); 
 element1.appendChild(element2);
 target.appendChild(element1); 
