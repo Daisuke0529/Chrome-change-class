f98c654ba63a2a0dfe0c3cae1088cc1e1344dc1f
Daisuke0529 <114968942+Daisuke0529@users.noreply.github.com>
2022-12-27 03:22:31 +0900

    a

--- tigers/content_script2.js
+++ tigers/content_script2.js
@@ -1,24 +1,50 @@
-let element1 = document.createElement("button"); 
-element1.classList.add("refresh");
-element1.onclick=buttonclick;
-let element2 =document.createElement("a");
-// element2.href="https://www.youtube.com/watch?v=Jb1_rM-qHzY&t=263s";
-element2.textContent = "3:大山悠輔";            
-let target = document.querySelector("h1"); 
-element1.appendChild(element2);
-target.appendChild(element1); 
+let button = document.createElement("button"); 
+button.id="refresh";
+button.onclick=buttonclick;
+let a =document.createElement("a");
+a.textContent = "3:大山悠輔";
+let audio = document.createElement("audio");
+audio.id="oyama";
+let source= document.createElement("source"); 
+source.src= "oyama.mp3";  
+source.type="audio/mp3"
+let h1 = document.querySelector("h1");
+audio.appendChild(source);
+button.appendChild(a); 
+button.appendChild(audio);
+h1.appendChild(button); 
+
+const music = new Audio('oyama.mp3');
+// music.play();
+var promise = document.querySelector('audio').play();
+
+if (promise !== undefined) {
+promise.then(_ => {
+    // Autoplay started!
+}).catch(error => {
+    // Autoplay was prevented.
+    // Show a "Play" button so that user can start playback.
+});
+}
 
 let count=0;
-const music = new Audio('大山悠輔　応援歌.mp3');
 function buttonclick(){
     alert('悠然と振り構えたバットに　我らの夢を乗せて　スタンドへはじき返せ　栄光つかむその日まで');
     count+=1;
-    music.play();
-    music.volume = 0.5;
-    music.loop = true;
+    // music.play();
+    // music.volume = 0.5;
+    // music.loop = true;
     if(count>2){
-        element2.href="https://www.youtube.com/watch?v=Jb1_rM-qHzY&t=289s";
-        element1.appendChild(element2);
-        target.appendChild(element1); 
+        a.href="https://www.youtube.com/watch?v=Jb1_rM-qHzY&t=289s";
+        button.appendChild(a);
+        h1.appendChild(button); 
     }
 }
+
+refresh.addEventListener('click', function(){
+    music.play();
+    if(music.muted){
+        music.muted = false;
+    }
+    music.loop = true;
+  });
